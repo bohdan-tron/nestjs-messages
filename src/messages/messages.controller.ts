7261f463e6fdc8ba6174cfdc233a88af69c99242
Bohdan Tron <bohdan.tron@gmail.com>
2025-01-31 07:51:16 +0200

    feat: add service and repo, w/o dependency injection still

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,34 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // there should be dependency injection, but practice
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return 'Hello World';
+    return this.messagesService.findAll();
   };
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return { id, content: 'Hello World' };
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
