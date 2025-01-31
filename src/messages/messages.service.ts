12ed770c756991f169c4bccb47913f611b2764b3
Bohdan Tron <bohdan.tron@gmail.com>
2025-01-31 10:40:20 +0200

    feat: add dependepcy injection, as part of Inversion of control technique

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,12 +1,9 @@
+import { Injectable } from "@nestjs/common";
 import { MessagesRepository } from "./messages.repository";
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    // there should be dependency injection, but practice
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
